d650e9e6a7557626fa6bb354eba431d55fdde2df
Ju Yeon Kwon <zuyeonkwon@gmail.com>
2025-09-15 04:58:50 +0900

    Delete Week 15/django directory

--- Week 15/django/mysite/skn/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SknConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'skn'
--- Week 15/django/mysite/skn/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def Hello(request):
-    html = "<h1> 안녕 </h1>"
-    return HttpResponse(html)
